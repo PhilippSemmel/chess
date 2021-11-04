9fd3543d059e0327ae7218b0396a2ce148344420
sniffle02 <forkzphoenix@t-online.de>
2021-11-04 21:43:55 +0100

    updated

--- tests/pieces/bishop_test.py
+++ tests/pieces/bishop_test.py
@@ -8,17 +8,17 @@ bishop2 = Bishop(21, False, board)
 
 
 class GeneralBishopConstructionTestCase(unittest.TestCase):
+    def test_bishop_is_subclass_of_piece(self):
+        self.assertTrue(issubclass(Bishop, Piece))
+
+    def test_piece_type_is_bishop_code(self):
+        self.assertEqual(2, bishop1._type)
+
     def test_piece_pos_is_given_value(self):
         self.assertEqual(35, bishop1._pos)
 
     def test_piece_pos_is_any_given_value(self):
         self.assertEqual(21, bishop2._pos)
-
-    def test_raises_value_error_if_pos_value_is_too_high(self):
-        self.assertRaises(ValueError, Bishop, 64, True, board)
-
-    def test_raises_value_error_is_pos_value_is_too_low(self):
-        self.assertRaises(ValueError, Bishop, -1, True, board)
 
     def test_piece_color_is_given_value(self):
         self.assertTrue(bishop1._white_piece)
@@ -26,22 +26,188 @@ class GeneralBishopConstructionTestCase(unittest.TestCase):
     def test_piece_color_is_any_given_value(self):
         self.assertFalse(bishop2._white_piece)
 
-    def test_raises_type_error_if_pos_is_not_int(self):
-        self.assertRaises(TypeError, Bishop, True, True, board)
 
-    def test_raises_type_error_if_white_piece_is_not_bool(self):
-        self.assertRaises(TypeError, Bishop, 1, 1, board)
+class BishopMoveGenerationTestCase(unittest.TestCase):
+    # general
+    def test_cannot_move_when_trapped_by_own_pieces(self):
+        bishop = Board('8/8/2PPP3/2PBP3/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
 
-    # def test_raises_type_error_if_board_is_not_board(self):
-    #     self.assertRaises(TypeError, Bishop, 1, True, 1)
+    def test_can_move_in_multiple_directions(self):
+        bishop = Board('8/1p6/8/5P2/1p2B2P/4P3/8/1P6 w - - 0 1').get_piece(28)
+        self.assertEqual({(28, 35), (28, 42), (28, 49), (28, 19), (28, 10), (28, 21), (28, 14), (28, 7)},
+                         bishop.pseudo_legal_moves)
 
+    # horizontal moves
+    # right
+    def test_can_move_one_step_right(self):
+        bishop = Board('8/8/2PPP3/2PB1P2/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
 
-class SpecificBishopConstructionTestCase(unittest.TestCase):
-    def test_queen_is_subclass_of_piece(self):
-        self.assertTrue(issubclass(Bishop, Piece))
+    def test_can_capture_one_step_right(self):
+        bishop = Board('8/8/2PPP3/2PBp3/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
 
-    def test_piece_type_is_queen_code(self):
-        self.assertEqual(2, bishop1._type)
+    # left
+    def test_can_move_one_step_left(self):
+        bishop = Board('8/8/4PPP1/3P1BP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left(self):
+        bishop = Board('8/8/4PPP1/4pBP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    # vertical moves
+    # up
+    def test_can_move_one_step_up(self):
+        bishop = Board('8/8/8/3P4/2P1P3/2PBP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_up(self):
+        bishop = Board('8/8/8/8/2PpP3/2PBP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    # down
+    def test_can_move_one_step_down(self):
+        bishop = Board('8/2PPP3/2PBP3/2P1P3/3P4/8/8/8 w - - 0 1').get_piece(43)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_down(self):
+        bishop = Board('8/5PPP/5PBP/5PpP/8/8/8/8 w - - 0 1').get_piece(46)
+        self.assertEqual(set(), bishop.pseudo_legal_moves)
+
+    # diagonal moves
+    # right up
+    def test_can_move_one_step_right_up(self):
+        bishop = Board('8/8/8/8/3P4/PP6/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_two_steps_right_up(self):
+        bishop = Board('8/8/8/4P3/8/PP6/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18), (9, 27)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_three_steps_right_up(self):
+        bishop = Board('8/8/5P2/8/8/PP6/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18), (9, 27), (9, 36)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_right_up(self):
+        bishop = Board('8/8/8/8/8/PP6/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18), (9, 27), (9, 36), (9, 45), (9, 54), (9, 63)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_right_up(self):
+        bishop = Board('8/2PP4/2PBP3/2PPP3/8/8/8/8 w - - 0 1').get_piece(43)
+        self.assertEqual({(43, 52), (43, 61)}, bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_right_up(self):
+        bishop = Board('8/8/8/8/8/PPp5/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_right_up(self):
+        bishop = Board('8/8/8/4p3/8/PP6/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 18), (9, 27), (9, 36)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_right_up(self):
+        bishop = Board('8/8/8/8/p7/5PP1/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 23)}, bishop.pseudo_legal_moves)
+
+    # right down
+    def test_can_move_one_step_right_down(self):
+        bishop = Board('PPP5/PBP5/PP6/3P4/8/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_two_steps_right_down(self):
+        bishop = Board('PPP5/PBP5/PP6/8/4P3/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42), (49, 35)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_three_steps_right_down(self):
+        bishop = Board('PPP5/PBP5/PP6/8/8/5P2/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42), (49, 35), (49, 28)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_right_down(self):
+        bishop = Board('PPP5/PBP5/PP6/8/8/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42), (49, 35), (49, 28), (49, 21), (49, 14), (49, 7)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_right_down(self):
+        bishop = Board('8/8/8/8/2PPP3/2PBP3/2PP4/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 12), (19, 5)}, bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_right_down(self):
+        bishop = Board('PPP5/PBP5/PPp5/8/8/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_right_down(self):
+        bishop = Board('PPP5/PBP5/PP6/8/4p3/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 42), (49, 35), (49, 28)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_right_down(self):
+        bishop = Board('5PPP/5PBP/5PP1/p7/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 47)}, bishop.pseudo_legal_moves)
+
+    # left down
+    def test_can_move_one_step_left_down(self):
+        bishop = Board('5PPP/5PBP/6PP/4P3/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_two_steps_left_down(self):
+        bishop = Board('5PPP/5PBP/6PP/8/3P4/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45), (54, 36)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_three_steps_left_down(self):
+        bishop = Board('5PPP/5PBP/6PP/8/8/2P5/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45), (54, 36), (54, 27)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_left_down(self):
+        bishop = Board('5PPP/5PBP/6PP/8/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45), (54, 36), (54, 27), (54, 18), (54, 9), (54, 0)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_left_down(self):
+        bishop = Board('8/8/8/8/4PPP1/4PBP1/5PP1/8 w - - 0 1').get_piece(21)
+        self.assertEqual({(21, 12), (21, 3)}, bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left_down(self):
+        bishop = Board('5PPP/5PBP/5pPP/8/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_left_down(self):
+        bishop = Board('5PPP/5PBP/6PP/8/3p4/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual({(54, 45), (54, 36), (54, 27)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_left_down(self):
+        bishop = Board('PPP5/PBP5/1PP5/8/7p/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual({(49, 40)}, bishop.pseudo_legal_moves)
+
+    # left up
+    def test_can_move_one_step_left_up(self):
+        bishop = Board('8/8/8/8/4P3/6PP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_two_steps_left_up(self):
+        bishop = Board('8/8/8/3P4/8/6PP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21), (14, 28)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_three_steps_left_up(self):
+        bishop = Board('8/8/2P5/8/8/6PP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21), (14, 28), (14, 35)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_left_up(self):
+        bishop = Board('8/8/8/8/8/6PP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21), (14, 28), (14, 35), (14, 42), (14, 49), (14, 56)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_left_up(self):
+        bishop = Board('8/5PP1/4PBP1/4PPP1/8/8/8/8 w - - 0 1').get_piece(45)
+        self.assertEqual({(45, 52), (45, 59)}, bishop.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left_up(self):
+        bishop = Board('8/8/8/8/8/5pPP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21)}, bishop.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_left_up(self):
+        bishop = Board('8/8/8/3p4/8/6PP/5PBP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual({(14, 21), (14, 28), (14, 35)}, bishop.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_left_up(self):
+        bishop = Board('8/8/8/8/8/1PP4p/PBP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual({(9, 16)}, bishop.pseudo_legal_moves)
 
 
 if __name__ == '__main__':
--- tests/pieces/rook_test.py
+++ tests/pieces/rook_test.py
@@ -8,40 +8,198 @@ rook2 = Rook(21, False, board)
 
 
 class GeneralRookConstructionTestCase(unittest.TestCase):
+    def test_rook_is_subclass_of_piece(self):
+        self.assertTrue(issubclass(Rook, Piece))
+
+    def test_piece_type_is_rook_code(self):
+        self.assertEqual(3, rook1._type)
+
     def test_piece_pos_is_given_value(self):
         self.assertEqual(35, rook1._pos)
 
     def test_piece_pos_is_any_given_value(self):
         self.assertEqual(21, rook2._pos)
 
-    def test_raises_value_error_if_pos_value_is_too_high(self):
-        self.assertRaises(ValueError, Rook, 64, True, board)
-
-    def test_raises_value_error_is_pos_value_is_too_low(self):
-        self.assertRaises(ValueError, Rook, -1, True, board)
-
     def test_piece_color_is_given_value(self):
         self.assertTrue(rook1._white_piece)
 
     def test_piece_color_is_any_given_value(self):
         self.assertFalse(rook2._white_piece)
+    
+    
+class RookMoveGenerationTestCase(unittest.TestCase):
+    # general
+    def test_cannot_move_when_trapped_by_own_pieces(self):
+        rook = Board('8/8/2PPP3/2PRP3/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
 
-    def test_raises_type_error_if_pos_is_not_int(self):
-        self.assertRaises(TypeError, Rook, True, True, board)
+    def test_can_move_in_multiple_directions(self):
+        rook = Board('8/1p6/8/5P2/1p2R2P/4P3/8/1P6 w - - 0 1').get_piece(28)
+        self.assertEqual({(28, 29), (28, 30), (28, 36), (28, 44), (28, 52), (28, 60), (28, 27), (28, 26), (28, 25)},
+                         rook.pseudo_legal_moves)
 
-    def test_raises_type_error_if_white_piece_is_not_bool(self):
-        self.assertRaises(TypeError, Rook, 1, 1, board)
+    # horizontal moves
+    # right
+    def test_can_move_one_step_right(self):
+        rook = Board('8/8/2PPP3/2PR1P2/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36)}, rook.pseudo_legal_moves)
 
-    # def test_raises_type_error_if_board_is_not_board(self):
-    #     self.assertRaises(TypeError, Rook, 1, True, 1)
+    def test_can_move_two_steps_right(self):
+        rook = Board('8/8/2PPP3/2PR2P1/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36), (35, 37)}, rook.pseudo_legal_moves)
 
+    def test_can_move_three_steps_right(self):
+        rook = Board('8/8/2PPP3/2PR3P/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36), (35, 37), (35, 38)}, rook.pseudo_legal_moves)
 
-class SpecificRookConstructionTestCase(unittest.TestCase):
-    def test_queen_is_subclass_of_piece(self):
-        self.assertTrue(issubclass(Rook, Piece))
+    def test_cannot_move_beyond_the_edge_of_the_board_right(self):
+        rook = Board('8/8/2PPP3/2PR4/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36), (35, 37), (35, 38), (35, 39)}, rook.pseudo_legal_moves)
 
-    def test_piece_type_is_queen_code(self):
-        self.assertEqual(3, rook1._type)
+    def test_can_move_from_any_position_right(self):
+        rook = Board('8/8/8/8/8/1PPP4/1PR5/1PPP4 w - - 0 1').get_piece(10)
+        self.assertEqual({(10, 11), (10, 12), (10, 13), (10, 14), (10, 15)}, rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_right(self):
+        rook = Board('8/8/2PPP3/2PRp3/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36)}, rook.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_right(self):
+        rook = Board('8/8/2PPP3/2PR2p1/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36), (35, 37), (35, 38)}, rook.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_right(self):
+        rook = Board('8/8/p1PPP3/2PR4/2PPP3/8/8/8 w - - 0 1').get_piece(35)
+        self.assertEqual({(35, 36), (35, 37), (35, 38), (35, 39)}, rook.pseudo_legal_moves)
+
+    # left
+    def test_can_move_one_step_left(self):
+        rook = Board('8/8/4PPP1/3P1RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36)}, rook.pseudo_legal_moves)
+
+    def test_can_move_two_steps_left(self):
+        rook = Board('8/8/4PPP1/2P2RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36), (37, 35)}, rook.pseudo_legal_moves)
+
+    def test_can_move_three_steps_left(self):
+        rook = Board('8/8/4PPP1/1P3RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36), (37, 35), (37, 34)}, rook.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_left(self):
+        rook = Board('8/8/4PPP1/5RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36), (37, 35), (37, 34), (37, 33), (37, 32)}, rook.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_left(self):
+        rook = Board('8/8/8/8/8/4PPP1/5RP1/4PPP1 w - - 0 1').get_piece(13)
+        self.assertEqual({(13, 12), (13, 11), (13, 10), (13, 9), (13, 8)}, rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left(self):
+        rook = Board('8/8/4PPP1/4pRP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36)}, rook.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_left(self):
+        rook = Board('8/8/4PPP1/2p2RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36), (37, 35), (37, 34)}, rook.pseudo_legal_moves)
+
+    def test_cannot_capture_beyond_the_board_left(self):
+        rook = Board('8/8/4PPPp/5RP1/4PPP1/8/8/8 w - - 0 1').get_piece(37)
+        self.assertEqual({(37, 36), (37, 35), (37, 34), (37, 33), (37, 32)}, rook.pseudo_legal_moves)
+
+    # vertical moves
+    # up
+    def test_can_move_one_step_up(self):
+        rook = Board('8/8/8/3P4/2P1P3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27)}, rook.pseudo_legal_moves)
+
+    def test_can_move_two_steps_up(self):
+        rook = Board('8/8/3P4/8/2P1P3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27), (19, 35)}, rook.pseudo_legal_moves)
+
+    def test_can_move_three_steps_up(self):
+        rook = Board('8/3P4/8/8/2P1P3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27), (19, 35), (19, 43)}, rook.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_up(self):
+        rook = Board('8/8/8/8/2P1P3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27), (19, 35), (19, 43), (19, 51), (19, 59)}, rook.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_up(self):
+        rook = Board('8/8/8/8/5P1P/5PRP/5PPP/8 w - - 0 1').get_piece(22)
+        self.assertEqual({(22, 30), (22, 38), (22, 46), (22, 54), (22, 62)}, rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_up(self):
+        rook = Board('8/8/8/8/2PpP3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27)}, rook.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_up(self):
+        rook = Board('8/8/3p4/8/2P1P3/2PRP3/2PPP3/8 w - - 0 1').get_piece(19)
+        self.assertEqual({(19, 27), (19, 35), (19, 43)}, rook.pseudo_legal_moves)
+
+    # down
+    def test_can_move_one_step_down(self):
+        rook = Board('8/2PPP3/2PRP3/2P1P3/3P4/8/8/8 w - - 0 1').get_piece(43)
+        self.assertEqual({(43, 35)}, rook.pseudo_legal_moves)
+
+    def test_can_move_two_steps_down(self):
+        rook = Board('8/2PPP3/2PRP3/2P1P3/8/3P4/8/8 w - - 0 1').get_piece(43)
+        self.assertEqual({(43, 35), (43, 27)}, rook.pseudo_legal_moves)
+
+    def test_can_move_three_steps_down(self):
+        rook = Board('8/2PPP3/2PRP3/2P1P3/8/8/3P4/8 w - - 0 1').get_piece(43)
+        self.assertEqual({(43, 35), (43, 27), (43, 19)}, rook.pseudo_legal_moves)
+
+    def test_cannot_move_beyond_the_edge_of_the_board_down(self):
+        rook = Board('8/2PPP3/2PRP3/2P1P3/8/8/8/8 w - - 0 1').get_piece(43)
+        self.assertEqual({(43, 35), (43, 27), (43, 19), (43, 11), (43, 3)}, rook.pseudo_legal_moves)
+
+    def test_can_move_from_any_position_down(self):
+        rook = Board('8/5PPP/5PRP/5P1P/8/8/8/8 w - - 0 1').get_piece(46)
+        self.assertEqual({(46, 38), (46, 30), (46, 22), (46, 14), (46, 6)}, rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_down(self):
+        rook = Board('8/5PPP/5PRP/5PpP/8/8/8/8 w - - 0 1').get_piece(46)
+        self.assertEqual({(46, 38)}, rook.pseudo_legal_moves)
+
+    def test_can_move_and_capture_three_steps_down(self):
+        rook = Board('8/5PPP/5PRP/5P1P/8/6p1/8/8 w - - 0 1').get_piece(46)
+        self.assertEqual({(46, 38), (46, 30), (46, 22)}, rook.pseudo_legal_moves)
+    
+    # diagonal moves
+    # right up
+    def test_can_move_one_step_right_up(self):
+        rook = Board('8/8/8/8/3P4/PP6/PRP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_right_up(self):
+        rook = Board('8/8/8/8/8/PPp5/PRP5/PPP5 w - - 0 1').get_piece(9)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    # right down
+    def test_can_move_one_step_right_down(self):
+        rook = Board('PPP5/PRP5/PP6/3P4/8/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_right_down(self):
+        rook = Board('PPP5/PRP5/PPp5/8/8/8/8/8 w - - 0 1').get_piece(49)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    # left down
+    def test_can_move_one_step_left_down(self):
+        rook = Board('5PPP/5PRP/6PP/4P3/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left_down(self):
+        rook = Board('5PPP/5PRP/5pPP/8/8/8/8/8 w - - 0 1').get_piece(54)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    # left up
+    def test_can_move_one_step_left_up(self):
+        rook = Board('8/8/8/8/4P3/6PP/5PRP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
+
+    def test_can_capture_one_step_left_up(self):
+        rook = Board('8/8/8/8/8/5pPP/5PRP/5PPP w - - 0 1').get_piece(14)
+        self.assertEqual(set(), rook.pseudo_legal_moves)
 
 
 if __name__ == '__main__':
