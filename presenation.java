a7357f0a3dbb369a39ec5f1e532cdbb82c522774
sniffle02 <forkzphoenix@t-online.de>
2021-12-06 16:52:41 +0100

    updated

--- presenation.java
+++
@@ -1,7 +0,0 @@
-for (int new_pos = this._pos + diff; new_pos < this._pos + ((m + 1) * diff); new_pos += diff)
-
-for (int i = 0; i < min(diffs.length, max_moves.length); i++) {
-    diff = diffs[i];
-    m = max_moves[i];
-    ...
-}
