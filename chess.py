7dc31928ab26f4e819747dcd246d9a8385380256
sniffle02 <forkzphoenix@t-online.de>
2021-12-12 10:35:13 +0100

    updated

--- chess.py
+++ chess.py
@@ -1,37 +1,84 @@
 from board import Board
-from UI import Display
-from player import Player
-from typing import Tuple
+from player import Player, HumanPlayer, ComPlayer
+from typing import Tuple, Union
 
 MOVE = Tuple[int, int]
 
 
-board = Board()
-print(board)
-w_player = Player(True, input('> Enter the name for the white player: '))
-b_player = Player(False, input('> Enter the name for the black player: '))
-active_player, inactive_player = (w_player, b_player) if board.white_to_move else (b_player, w_player)
+class Game:
+    def __init__(self) -> None:
+        self._board: Board = Board()
+        self._w_player: Union[None, Player] = None
+        self._b_player: Union[None, Player] = None
 
+    """
+    initialization
+    """
+    def _init_players(self) -> None:
+        """
+        initialize the player objects
+        """
+        self._w_player = self._create_player('white')
+        self._b_player = self._create_player('black')
 
-def main():
-    while True:
-        moves = board.legal_moves
-        move = active_player.get_move(moves)
-        board.make_move(move)
-        print(board)
-        if board.checkmate:
-            print(active_player.name + ' wins.')
-            break
-        if board.stalemate:
-            print('Stalemate.')
-            break
-        new_turn()
+    @staticmethod
+    def _create_player(color: str) -> Player:
+        """
+        create a new player object
+        :param color: need to be "white" if player is supposed to be white otherwise "black"
+        :return: player object
+        """
+        while True:
+            input_ = input(f'> Is the {color} player a human player? [y]es/[n]o: ')
+            if input_ == 'y':
+                return HumanPlayer(color == 'white', input(f'> Enter the name of the {color} human player: '))
+            elif input_ == 'n':
+                return ComPlayer(color == 'white', input(f'> Enter the name of the {color} com player: '))
 
+    """
+    getters
+    """
+    @property
+    def _active_player(self) -> Player:
+        """
+        get the active player
+        :return: active player object
+        """
+        return self._w_player if self._board.white_to_move else self._b_player
 
-def new_turn():
-    global active_player, inactive_player
-    active_player, inactive_player = inactive_player, active_player
+    @property
+    def _inactive_player(self) -> Player:
+        """
+        get the inactive player
+        :return: inactive player object
+        """
+        return self._b_player if self._board.white_to_move else self._w_player
+
+    """
+    game loop
+    """
+    def main(self) -> None:  # tests & doc & new method: game_over -> bool
+        self._init_players()
+        print(self._board)
+        while True:
+            moves = self._board.legal_moves()
+            move = self._active_player.get_move(moves)
+            self._board.make_move(move)
+            print(self._board)
+            if self._board.checkmate:
+                print(self._active_player.name + ' wins.')
+                break
+            if self._board.stalemate:
+                print('Stalemate.')
+                break
+            if self._board.half_move_clock > 50:
+                print('Reicht jetzt auch.')
+                break
+            if len(self._board.pieces) <= 2:
+                print('REMIS!')
+                break
 
 
 if __name__ == '__main__':
-    main()
+    game = Game()
+    game.main()
